99e857c208b87ed14198fe3d9d11d6dae78a5518
Zeeshan Abbasi <sociospherez@gmail.com>
2025-11-23 01:50:25 +0000

    added better images to the roadmap apge

--- projects/fuelupuk/obj/Debug/net8.0/FuelUp.Notify.AssemblyInfo.cs
+++ projects/fuelupuk/obj/Debug/net8.0/FuelUp.Notify.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FuelUp.Notify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff330ab7cd77e643e414d543711fbb8d711b48c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0c5622e9a5d624f5decc3da4c7a2d83971c2df")]
 [assembly: System.Reflection.AssemblyProductAttribute("FuelUp.Notify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FuelUp.Notify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
